adf674b0513ab3ae478ebe162474bc8d32bd95f2
Peter Cortez <innov.petercortez@gmail.com>
2020-09-24 09:42:25 +0800

    Fix network issues with provisioner

--- common-provisioners/00-utils.sh
+++ common-provisioners/00-utils.sh
@@ -6,6 +6,12 @@ echo "|---------------------------------------------"
 echo "| Updating your repositories..."
 echo "|---------------------------------------------"
 echo "*/"
+# The next few lines will disable ipv6 so we don't encounter networking issues
+# introduced by it while provisioning.
+echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
+sysctl -p
 apt-get update
 
 echo "/*"
